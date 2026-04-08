d3fc2ad20418cb8bb1ba9048bb6b3bc43585c8c9
Marek Vician <marc.vician@gmail.com>
2026-04-08 09:52:37 +0200

    Updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
-REPO_DIR="~/buntslido"
+REPO_DIR="$(cd "$(dirname "$0")" && pwd)"
+# REPO_DIR="~/buntslido"
 
 echo "==> Pulling latest code..."
 git -C "$REPO_DIR" pull
